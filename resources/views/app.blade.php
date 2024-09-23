fdfa2023657be5ad408606948f8d185abfe8c255
Gavin Tynan <tynangav@gmail.com>
2024-09-23 18:21:32 -0400

    fix component rendering

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
     <!-- Styles -->
     @vite(['resources/js/app.js', 'resources/css/app.css']) <!-- Load your Vue components and styles -->
 
