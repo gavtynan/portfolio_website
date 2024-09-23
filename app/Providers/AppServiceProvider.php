efadb366a3d92efd0ba7b5ab8fb2f9c401aec253
Gavin Tynan <tynangav@gmail.com>
2024-09-23 18:39:36 -0400

    Back to default, finding new solution

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('production') || $this->app->environment('dev')) {
-            URL::forceScheme('https');
-        }
     }
 
     /**
