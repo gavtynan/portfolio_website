1426a5ae79b5be70e26160bce5e3f8fd55ac5aff
Gavin Tynan <tynangav@gmail.com>
2024-09-23 18:55:01 -0400

    lowkey making progress

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,7 @@
     
 
     <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <style src="{{ secure_asset('css/app.css') }}" defer></script>
 
     <!-- Styles -->
     @vite(['resources/js/app.js', 'resources/css/app.css']) <!-- Load your Vue components and styles -->
