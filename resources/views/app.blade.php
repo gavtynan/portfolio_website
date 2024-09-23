a33dd641d0f3d6d5efa3292ca012de8731a0dad0
Gavin Tynan <tynangav@gmail.com>
2024-09-23 18:56:33 -0400

    retard typo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
     
 
     <script src="{{ secure_asset('js/app.js') }}" defer></script>
-    <style src="{{ secure_asset('css/app.css') }}" defer></script>
+    <style src="{{ secure_asset('css/app.css') }}" defer></style>
 
     <!-- Styles -->
     @vite(['resources/js/app.js', 'resources/css/app.css']) <!-- Load your Vue components and styles -->
