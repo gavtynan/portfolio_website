0aceb11a4d770a989d337d1870bb8f9025cc863c
Gavin Tynan <tynangav@gmail.com>
2024-09-23 17:46:45 -0400

    fix vite config (for now)

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,27 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ["resources/js/app.js", "resources/css/app.css"],
-            refresh: true,
-        }),
+        laravel(["resources/js/app.js"]),
         vue({
             template: {
                 transformAssetUrls: {
+                    // The Vue plugin will re-write asset URLs, when referenced
+                    // in Single File Components, to point to the Laravel web
+                    // server. Setting this to `null` allows the Laravel plugin
+                    // to instead re-write asset URLs to point to the Vite
+                    // server instead.
                     base: null,
+
+                    // The Vue plugin will parse absolute URLs and treat them
+                    // as absolute paths to files on disk. Setting this to
+                    // `false` will leave absolute URLs un-touched so they can
+                    // reference assets in the public directory as expected.
                     includeAbsolute: false,
                 },
             },
         }),
     ],
-    resolve: {
-        alias: {
-            vue: "vue/dist/vue.esm-bundler.js",
-        },
-    },
-    server: {
-        port: 5173,
-        hmr: {
-            host: "localhost",
-        },
-    },
-    build: {
-        // Output to 'public/js' and 'public/css' directories
-        rollupOptions: {
-            output: {
-                // Define where JS files will go
-                entryFileNames: "js/[name].js",
-                chunkFileNames: "js/[name].js",
-                assetFileNames: ({ name }) => {
-                    if (name && name.endsWith(".css")) {
-                        // Define where CSS files will go
-                        return "css/[name].css";
-                    }
-                    return "assets/[name][extname]";
-                },
-            },
-        },
-        outDir: "public", // Ensure output goes to the public directory
-        emptyOutDir: false, // Clear previous builds
-        manifest: true, // Enable manifest generation
-    },
-    manifest: {
-        fileName: "manifest.json", // Name of the manifest file
-        outDir: "public/build", // Set manifest to output in 'public/build'
-    },
 });
